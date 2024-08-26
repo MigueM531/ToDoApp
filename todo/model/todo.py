c5a53367754924d7a0a0c9676546f141256090d2
valeria <valeria>
2024-08-26 13:14:35 -0500

    created class TodoBooks

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -18,3 +18,32 @@ class Todo:
     def __str__(self) -> str:
         return f"{self.code_id} - {self.title}"
 
+
+class TodoBook:
+
+    def __init__(self):
+        self.todos = {}
+
+    def add_todo(self, title: str, description: str) -> int:
+        todo_id = len(self.todos) + 1
+        new_todo = Todo(todo_id, title, description)
+        self.todos[todo_id] = new_todo
+        return todo_id
+
+    def pending_todos(self) -> list:
+        return [todo for todo in self.todos.values() if not todo.completed]
+
+    def completed_todos(self) -> list:
+        return [todo for todo in self.todos.values() if todo.completed]
+
+    def tags_todo_count(self) -> dict[str, int]:
+        all_tags = [tag for todo in self.todos.values() for tag in todo.tags]
+
+        tag_count = {}
+
+        for tag in all_tags:
+            if tag in tag_count:
+                tag_count[tag] += 1
+            else:
+                tag_count[tag] = 1
+        return tag_count
