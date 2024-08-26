844bc9813c8c349f8515121669bea5d68dfab8c4
valeria <valeria>
2024-08-26 13:13:04 -0500

    created class Todo

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,20 @@
 # TODO: Add code here
+class Todo:
+
+    def __init__(self, code_id: int, tittle: str, description: str):
+        self.code_id: int = code_id
+        self.title: str = tittle
+        self.description: str = description
+        self.completed: bool = False
+        self.tags: list[str] = []
+
+    def mark_completed(self):
+        self.completed = True
+
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def __str__(self) -> str:
+        return f"{self.code_id} - {self.title}"
+
